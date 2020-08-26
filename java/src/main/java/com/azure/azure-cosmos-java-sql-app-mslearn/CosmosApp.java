714c9716bd4bf7e240e25bae0cb26934b22c7aca
Andrew Feldman <andrew.feldman@microsoft.com>
2020-08-26 05:32:45 -0700

    Structure of repo

---
+++ java/src/main/java/com/azure/azure-cosmos-java-sql-app-mslearn/CosmosApp.java
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.azure-cosmos-java-sql-app-mslearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CosmosApp {
+
+    protected static Logger logger = LoggerFactory.getLogger(CosmosApp.class.getSimpleName());
+
+    public static void main(String[] args) {
+	logger.info("Hello world.");
+    }
+}
