5e07100d1e7ef8fcb20a1141f5f6b33e1ff21669
Andrew Feldman <andrew.feldman@microsoft.com>
2020-09-14 02:50:58 -0700

    Call read code

--- java/src/main/java/com/azure/cosmos/examples/mslearnbasicapp/CosmosApp.java
+++ java/src/main/java/com/azure/cosmos/examples/mslearnbasicapp/CosmosApp.java
@@ -142,6 +142,7 @@ public final class CosmosApp {
         );
 
         createUserDocumentsIfNotExist(new ArrayList(Arrays.asList(maxaxam, nelapin)));
+        readUserDocument(maxaxam);
         client.close();
     }
 
